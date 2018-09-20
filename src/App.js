23a68f48b65a94f190e29b365006b62fe4c41f54
Hannah Zulueta <hannahjoy.zulueta@gmail.com>
2018-09-20 13:58:55 -0700

    Prettify

--- src/App.js
+++ src/App.js
@@ -1,87 +1,94 @@
-import React from 'react'
-import { Checkbox, Repeatable, Text, Textarea } from './components'
-import api from './mockApi'
+import React from 'react';
+import { Checkbox, Repeatable, Text, Textarea } from './components';
+import api from './mockApi';
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       data: {
         title: '',
         rating: 0,
         year: null,
         description: '',
-        upcoming: true, 
-        cast: [],
+        upcoming: true,
+        cast: []
       }
-    }
+    };
 
-    this.handleChange = this.handleChange.bind(this)
-    this.handleUpdate = this.handleUpdate.bind(this)
-    this.Input = this.Input.bind(this)
+    this.handleChange = this.handleChange.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
+    this.Input = this.Input.bind(this);
   }
 
   /**
-   * 
+   *
    */
   handleChange(delta) {
-    this.setState(({ data }) => ({ data: { ...data, ...delta }}))
+    this.setState(({ data }) => ({ data: { ...data, ...delta } }));
   }
 
   /**
-   * 
+   *
    */
   async handleUpdate(publish = false) {
-    const { data } = this.state
-    const results = await api.post({ ...data, publish })
-    console.log('Content updated!')
-    return results
+    const { data } = this.state;
+    const results = await api.post({ ...data, publish });
+    console.log('Content updated!');
+    return results;
   }
 
   /**
-   * 
+   *
    */
   Input({ children, iterable, label, id }) {
     const handleChange = value => {
-      this.handleChange({ [id]: value })
-    }
-    const value = this.state.data[id]
-    let props = {}
+      this.handleChange({ [id]: value });
+    };
+    const value = this.state.data[id];
+    let props = {};
 
-    if(iterable) {
+    if (iterable) {
       props = {
         id,
         value,
-        onCreate: (item) => handleChange([...value, {
-          ...item,
-          id: Math.floor(Math.random() * 100000),
-        }]),
-        onUpdate: (item) => handleChange(value.map(prev => {
-          if(item.id === prev.id) {
-            return item
-          }
-          return prev
-        })),
-        onDelete: (id) => handleChange(value.filter(prev => prev.id !== id))
-      }
+        onCreate: item =>
+          handleChange([
+            ...value,
+            {
+              ...item,
+              id: Math.floor(Math.random() * 100000)
+            }
+          ]),
+        onUpdate: item =>
+          handleChange(
+            value.map(prev => {
+              if (item.id === prev.id) {
+                return item;
+              }
+              return prev;
+            })
+          ),
+        onDelete: id => handleChange(value.filter(prev => prev.id !== id))
+      };
     } else {
       props = {
         id,
         value,
         onBlur: () => this.handleUpdate(false),
-        onChange: e => handleChange(e.target.value),
-      }
+        onChange: e => handleChange(e.target.value)
+      };
     }
     return (
       <div className="Form-Group">
         <div className="Form-Label">{label}</div>
         {children(props)}
       </div>
-    )
+    );
   }
 
   render() {
-    const { Input } = this
+    const { Input } = this;
     return (
       <div className="Form">
         <Input label="Title" id="title">
@@ -96,12 +103,10 @@ class App extends React.Component {
         <Input label="Cast" iterable id="cast">
           {props => <Repeatable {...props} />}
         </Input>
-        <button onClick={() => this.handleUpdate(true)}>
-          {'Publish'}
-        </button>
+        <button onClick={() => this.handleUpdate(true)}>{'Publish'}</button>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
