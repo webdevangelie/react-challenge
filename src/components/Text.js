f561b23a8a65141fda19c64f2e20ced570e900be
Hannah Zulueta <hannahjoy.zulueta@gmail.com>
2018-09-21 12:04:19 -0700

    Refactor to accept type prop

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,3 +1,5 @@
-import React from 'react'
+import React from 'react';
 
-export default props => <input {...props} value={props.value || '' } type="text" />
+export default props => (
+  <input {...props} value={props.value || ''} type={props.type} />
+);
