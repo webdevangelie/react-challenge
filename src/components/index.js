62c424cb0edbc5439372648f7e2b7118a47ea933
Hannah Zulueta <hannahjoy.zulueta@gmail.com>
2018-09-23 13:34:35 -0700

    Create Message component

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,8 @@
-import Checkbox from './Checkbox'
-import Repeatable from './Repeatable'
-import Select from './Select'
-import Text from './Text'
-import Textarea from './Textarea'
+import Checkbox from './Checkbox';
+import Repeatable from './Repeatable';
+import Select from './Select';
+import Text from './Text';
+import Textarea from './Textarea';
+import Message from './Message';
 
-export {
-  Checkbox,
-  Repeatable,
-  Select,
-  Text,
-  Textarea,
-}
+export { Checkbox, Repeatable, Select, Text, Textarea, Message };
